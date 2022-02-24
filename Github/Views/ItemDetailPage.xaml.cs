ab9ef88334397d1516501927cf14439aef9f7ad6
Espyn <taylore1@findlay.edu>
2022-02-23 21:00:46 -0500

    Add project files.

---
+++ Github/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Github.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Github.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
